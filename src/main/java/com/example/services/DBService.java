35b1b7c0180072e5faefe3c5d9cd0b3ddc68757a
ximenes21 <peximenes21@gmail.com>
2024-10-02 20:50:29 -0300

    vamo ve

--- src/main/java/com/example/services/DBService.java
+++ src/main/java/com/example/services/DBService.java
@@ -21,13 +21,13 @@ public class DBService {
 
     public void initDB(){
 
-        Medico medico01 = new Medico(0, "Evandro Silvares", LocalDate.of(2024,9,10), new BigDecimal("15000.00"));
-        Medico medico02 = new Medico(0, "Ricardo Jesus", LocalDate.of(2024,9,24), new BigDecimal("12000.00"));
-        Medico medico03 = new Medico(0, "Sergio De Souza", LocalDate.of(2024,9,20), new BigDecimal("22000.00"));
+        Medico medico01 = new Medico(0, "Carlinhos Brown", LocalDate.of(2000,7,10), new BigDecimal("15000.00"));
+        Medico medico02 = new Medico(0, "Sandrinho da Marmoraria", LocalDate.of(2002,3,24), new BigDecimal("12000.00"));
+        Medico medico03 = new Medico(0, "Sergio De Souza", LocalDate.of(1998,7,20), new BigDecimal("22000.00"));
 
-        Paciente paciente01 = new Paciente(0, "Carlos de Souza", LocalDate.of(1980,9,25), new BigDecimal("5000.00"));
-        Paciente paciente02 = new Paciente(0, "Bianca Cardoso", LocalDate.of(1990,10,30), new BigDecimal("3000.00"));
-        Paciente paciente03 = new Paciente(0, "Beatriz Miranda", LocalDate.of(2000,6,14), new BigDecimal("8000.00"));
+        Paciente paciente01 = new Paciente(0, "Pedro Ximenes", LocalDate.of(1980,9,25), new BigDecimal("8000.00"));
+        Paciente paciente02 = new Paciente(0, "Fabinho Cardoso", LocalDate.of(1990,10,30), new BigDecimal("8000.00"));
+        Paciente paciente03 = new Paciente(0, "Roger Guedes", LocalDate.of(2000,6,14), new BigDecimal("8000.00"));
 
         medicoRepo.save(medico01);
         medicoRepo.save(medico02);
